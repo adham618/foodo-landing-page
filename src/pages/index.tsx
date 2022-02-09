4c8a9fbcec7465cf149026100cd0d724a07c253c
adham618 <adhamtarek291@gmail.com>
2022-02-09 13:35:45 +0200

    initial comment

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>Foodo Landing-Page</title>
+        <meta name="description" content="Foodo free landing-page for food app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="bg-red-500">
+        <h1>hhhhh</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
