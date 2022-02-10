adf7fb2bcd7a1bfaf0ad99790e77a39cf3a9ce69
adham618 <adhamtarek291@gmail.com>
2022-02-10 15:02:28 +0200

    complete About Component

--- src/common/components/About/About.tsx
+++ src/common/components/About/About.tsx
@@ -1,11 +1,36 @@
+/* eslint-disable @next/next/no-img-element */
+import { Col, Container, Row } from "react-bootstrap"
+
 interface AboutProps {
 
 }
 
 const About = ({ }: AboutProps) => {
   return (
-    <div>
-
+    <div className="about-container-bg">
+      <Container>
+        <Row>
+          <Col xs={12} md={6}>
+            <img src="assets/image-about.png" className="img-fluid" alt="about-image" />
+          </Col>
+          <Col xs={12} md={6} className="about-details">
+            <div>
+              <h2>Best Offer In Town</h2>
+              <b>Lorem ipsum, or lipsum as it is sometimes known,</b>
+              <br />
+              <br />
+              <p>
+                Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+                used in laying out print, graphic or web designs. The passage is
+                attributed to an unknown typesetter in the 15th century who is
+                thought to have scrambled parts of Cicero&apos;s De Finibus Bonorum
+                et Malorum for use in a type specimen book.
+              </p>
+              <button className="btn btn-custom">Explore More</button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
